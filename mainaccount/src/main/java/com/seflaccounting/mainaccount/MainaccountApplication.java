5d4a5f54e773e60716a3ca104129a45466586cfe
Vitaly <woodanon123@gmail.com>
2022-06-11 13:26:16 +0500

    add mainacc

---
+++ mainaccount/src/main/java/com/seflaccounting/mainaccount/MainaccountApplication.java
@@ -0,0 +1,13 @@
+package com.seflaccounting.mainaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainaccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainaccountApplication.class, args);
+    }
+
+}
---
+++ mainaccount/src/test/java/com/seflaccounting/mainaccount/MainaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seflaccounting.mainaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainaccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
